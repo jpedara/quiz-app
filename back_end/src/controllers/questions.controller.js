3e68dfb7083d0585d8a458b24481b0c29a151d1b
Jyothi Prakash Edara <prakashchowdaryedara@gmail.com>
2023-08-22 00:47:34 +0530

    Backend: Added logic to retreive random questions

--- back_end/src/controllers/questions.controller.js
+++ back_end/src/controllers/questions.controller.js
@@ -6,7 +6,22 @@ const {questions} = require(path.join(__dirname,'../models','questions'));
  */
 const getRandomQuestions = (req,res,next)=>{
     try{
-        return res.json({});
+        const randomQuestionsCount = 5;
+        if(questions.length <=5){
+            return res.json(questions);
+        }else{
+            let resultObj = {}
+            count = 0;
+            while( true ){
+                const idx = Math.floor(Math.random()*questions.length);
+                if(!resultObj[idx]){
+                    resultObj[idx] = questions[idx];
+                    count++;
+                }
+                if (count === randomQuestionsCount) break
+            }
+            return res.json(Object.values(resultObj));
+        }
     }
     catch(error){
         next(error);
